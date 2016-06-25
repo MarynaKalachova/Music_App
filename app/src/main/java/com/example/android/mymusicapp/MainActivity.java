51b8eb5a4dd30020ccfbf19c9a1a31c6aa817e37
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-06-25 16:48:13 -0400

    Add OfflineActivity, FavoritesActivity, RadioActivity and RecentActivity.

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -16,12 +16,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView offline = (TextView) findViewById(R.id.offline_category);
-
         offline.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent offlineIntent = new Intent (MainActivity.this, OfflineActivity.class);
                 startActivity(offlineIntent);
+            }
+        });
+
+        TextView online = (TextView) findViewById(R.id.online_category);
+        online.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent onlineIntent = new Intent (MainActivity.this, OnlineActivity.class);
+                startActivity(onlineIntent);
+            }
+        });
+
+        TextView radio = (TextView) findViewById(R.id.radio_category);
+        radio.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent radioIntent = new Intent(MainActivity.this, RadioActivity.class);
+                startActivity(radioIntent);
+            }
+        });
+
+        TextView recent = (TextView) findViewById(R.id.recent_category);
+        recent.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recentIntent = new Intent(MainActivity.this, RecentActivity.class);
+                startActivity(recentIntent);
+            }
+        });
+
+        TextView favorites = (TextView) findViewById(R.id.favorites_category);
+        favorites.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent favoritesIntent = new Intent(MainActivity.this, FavoritesActivity.class);
+                startActivity(favoritesIntent);
             }
         });
     }
