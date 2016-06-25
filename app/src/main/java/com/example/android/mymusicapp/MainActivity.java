2ee41c8feb906eab588af31089625b7e8a599c85
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-06-25 16:28:36 -0400

    Add OfflineActivity and set OnClickListener

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.mymusicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView offline = (TextView) findViewById(R.id.offline_category);
+
+        offline.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent offlineIntent = new Intent (MainActivity.this, OfflineActivity.class);
+                startActivity(offlineIntent);
+            }
+        });
     }
 }
